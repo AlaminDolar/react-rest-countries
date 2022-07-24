3985876d17b95e7cba46c2c9c205e31b3c92a7d0
AlaminDolar <amin15-9898@diu.edu.bd>
2022-07-24 14:03:48 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import Countries from "./components/Countries/Countries";
+
 
 function App() {
   return (
@@ -9,33 +9,35 @@ function App() {
   );
 }
 
-function Countries() {
-  const [countries, setCountries] = useState([]);
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(res => res.json())
-      .then(data => setCountries(data));
-
-  }, []);
-  return (
-    <div>
-      <h2>Travelling In Morjwad</h2>
-      <h3>Countries: {countries.length}</h3>
-
-      {
-        countries.map(country => <Country name={country.name.common}></Country>)
-      }
-
-    </div>)
-}
-
-
-function Country(props) {
-  return (
-    <div>
-      <h2>Nmae : {props.name}</h2>
-    </div>
-  )
-}
 
 export default App;
+
+// //
+// function Countries() {
+//   const [countries, setCountries] = useState([]);
+//   useEffect(() => {
+//     fetch('https://restcountries.com/v3.1/all')
+//       .then(res => res.json())
+//       .then(data => setCountries(data));
+
+//   }, []);
+//   return (
+//     <div>
+//       <h2>Travelling In Morjwad</h2>
+//       <h3>Countries: {countries.length}</h3>
+
+//       {
+//         countries.map(country => <Country name={country.name.common} capital={country.capital}></Country>)
+//       }
+
+//     </div>)
+// }
+
+
+// function Country(props) {
+//   return (
+//     <div>
+//       <h2>Nmae : {props.name}</h2>
+//       <p> Capital: {props.capital}</p>
+//     </div>
+//   )
---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,10 @@
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Hello from Countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
